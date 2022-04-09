2b42c8c7d527820339ed4eb8fa02b76a9c4ed144
Ankita Suman <ankitasuman8667@gmail.com>
2022-04-09 08:31:05 +0530

    104 / 104 test cases passed.
    Example 1:
    
    Input: nums = [3,4,5,2]
    Output: 12 
    Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
    Example 2:
    
    Input: nums = [1,5,4,5]
    Output: 16
    Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
    Example 3:
    
    Input: nums = [3,7]
    Output: 12

---
+++ LeetCode Challenges/Maximum Product of Two Elements in an Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        if(nums.length==2) return (nums[0]-1)*(nums[1]-1);
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+         for(int i=0;i<nums.length;i++){
+             if(nums[i]>max1) {
+                 max2 = max1; max1 = nums[i];
+             }
+             else if(nums[i]>max2 && nums[i]<=max1) max2 = nums[i];
+         }
+        return ((max1-1)*(max2-1));
+    }
+}
