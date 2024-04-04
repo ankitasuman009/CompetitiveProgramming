790ce219965c1c50bed126169dd2acd7d81f1806
Ankita Suman <ankitasuman8667@gmail.com>
2024-04-04 14:03:35 +0530

    11/11

---
+++ HackerRank/Caesar Cipher.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'caesarCipher' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER k
+     */
+
+    public static String caesarCipher(String s, int k) {
+    // Write your code here
+    StringBuilder str = new StringBuilder();
+    for(int i=0; i<s.length();i++){
+        if(s.charAt(i)>='a' && s.charAt(i)<='z'){
+            str.append((char)(((s.charAt(i)-'a')+k)%26+97));
+        }
+        else if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
+            str.append((char)(((s.charAt(i)-'A')+k)%26+65));
+        }
+        else str.append(s.charAt(i));
+    }
+    return str.toString();
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.caesarCipher(s, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
