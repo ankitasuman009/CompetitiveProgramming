e27e5e199d433eed7b6f6fe7b8f8a60059673ce1
Ankita Suman <ankitasuman8667@gmail.com>
2024-04-04 14:09:32 +0530

    12/12

---
+++ HackerRank/Plus Minus.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+    // Write your code here
+    int posi = 0, negi = 0, zero = 0;
+for(int i=0;i<arr.size();i++){
+    if(arr.get(i)>0) posi++;
+    else if(arr.get(i)<0) negi++;
+    else if(arr.get(i)==0) zero++;
+}
+
+System.out.format("%.6f\n%.6f\n%.6f\n", (float)posi/arr.size(), (float)negi/arr.size(), (float)zero/arr.size());
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
