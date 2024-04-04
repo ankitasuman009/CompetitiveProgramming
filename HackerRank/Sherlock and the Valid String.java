6ecdd90280cbe281a64c569cee97b8a2590f7041
Ankita Suman <ankitasuman8667@gmail.com>
2024-04-04 14:07:24 +0530

    20/20

---
+++ HackerRank/Sherlock and the Valid String.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'isValid' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String isValid(String s) {
+    // Write your code here
+    HashMap<Character, Integer> h = new HashMap<>();
+    for(char c: s.toCharArray())
+    h.put(c, h.getOrDefault(c, 0)+1);
+    int count = 0;
+    int compare = h.get(s.charAt(0));
+    for(char c: h.keySet())
+    if(compare!=h.get(c)) count++;
+return (count>1?"NO":"YES");
+    }
+    
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.isValid(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
